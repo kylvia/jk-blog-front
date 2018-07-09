09c505e24a5c2a37113f51472c92bd2acb500a9a
SP0006 <hh.todd@qq.com>
2018-07-09 17:37:43 +0800

    前端展示

---
+++ src/components/MessageBoard/vTextarea.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="input-con">
+    <span>留言内容:</span>
+    <!--ref 被用来给元素或子组件注册引用信息。
+    引用信息将会注册在父组件的 $refs 对象上。
+    如果在普通的 DOM 元素上使用，引用指向的就是 DOM 元素；
+    如果用在子组件上，引用就指向组件实例：  -->
+    <textarea :ref="message" type="text" :value="value" @input="updateVal" placeholder="请输入留言" cols="30" rows="10"></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vTextarea',
+  // 接收父组件传入的value
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    // 监听input值
+    updateVal: function (e) {
+      this.$emit('input', e.target.value)
+    },
+    focus: function () {
+      // 一个对象，持有注册过ref特性的所有DOM元素和组件实例。
+      this.$ref.message.focus()
+    }
+  },
+  data () {
+    return this.$store.state.app.sidebar
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .input-con{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  span{
+    width: 100px;
+    display: inline-block;
+    text-align: right;
+    vertical-align: top;
+  }
+  textarea{
+    border: 1px solid #e6e6ee;
+    padding: 4px 5px;
+    line-height: 24px;
+    border-radius: 5px;
+    width: 300px;
+    height: 100px;
+  }
+</style>
--- src/views/articleList/detail.vue
+++ src/views/articleList/detail.vue
@@ -12,6 +12,16 @@
           <div v-html='details.content'></div>
         </div>
       </div>
+      <div>
+        <div class="message">
+          <v-input v-model="username"></v-input>
+          <v-textarea v-model="message" ref="message"></v-textarea>
+          <div class="btn-con">
+            <input type="button" @click="handleSend" class="btn" value="发布">
+          </div>
+        </div>
+        <v-list :list="details.list" @reply="handleReply"></v-list>
+      </div>
       <el-tooltip placement="top" content="返回顶部">
         <back-to-top transitionName="fade" :customStyle="myBackToTopStyle" :visibilityHeight="300" :backPosition="0"></back-to-top>
       </el-tooltip>
@@ -21,11 +31,21 @@
 <script>
 import { parseTime } from '@/utils'
 import BackToTop from '@/components/BackToTop'
+import vInput from "@/components/MessageBoard/vInput"
+import vList from "@/components/MessageBoard/vList"
+import vTextarea from "@/components/MessageBoard/vTextarea"
 export default {
   name: 'detail',
-  components: { BackToTop },
+  components: {
+    BackToTop,
+    vInput,
+    vTextarea,
+    vList
+  },
   data() {
     return {
+      username: '',
+      message: '',
       details: {
         id: 1,
         timestamp: new Date(1526262855 * 1000),
@@ -33,7 +53,8 @@ export default {
         articleType: 0,
         title: '做了一个网页版的串口调试助手',
         content: '最近这两天在研究如何实现web页面和串口间通信，在网上也查了各种资料，electron、nw或者chrome serial，发现对于我来说都太难实现了，一来可用的资料太少，二来安装东西老是出问题，算了还是放弃吧，自己用常用且最熟悉的方式来实现吧，作为一名前端码农，选用的肯定是node作为服务器了，然后网页请求方式用ajax或websocket都可以，实现方式下文都有。',
-        pageviews: 23
+        pageviews: 23,
+        list:[]
       },
 
       myBackToTopStyle: {
@@ -71,6 +92,29 @@ export default {
       } else {
         return '转'
       }
+    }
+  },
+  methods: {
+    handleSend: function () {
+      if (this.username === ''){
+        this.$alert('请输入昵称', '提示')
+        return
+      }
+      if (this.message === '') {
+        this.$alert('请输入留言内容', '提示')
+        return
+      }
+      // 数组list存储了所有的留言内容，通过函数给list添加一项留言数据，添加成功后把文本框置空
+      this.list.push({
+        name: this.username,
+        message: this.message
+      })
+      this.message = ''
+    },
+    handleReply: function (index) {
+      var name = this.list[index].name
+      this.message = '回复@' + name + ':'
+      this.$ref.message.focus()
     }
   }
 }
@@ -156,5 +200,31 @@ export default {
     padding-top: 30px;
     background: #efefef;
     text-align: center;
+  }
+  .message {
+    display: flex;
+    flex-direction: column;
+  }
+  .message div{
+    margin-bottom: 12px;
+    flex-direction: row;
+  }
+  .message input:focus,
+  .message textarea:focus{
+    border: 1px solid #3399ff;
+    border-radius: 5px;
+  }
+  .message .btn-con{
+    text-align: center;
+    display: inline-block;
+  }
+  .message .btn{
+    padding: 6px 15px;
+    border: 1px solid #39f;
+    border-radius: 4px;
+    color: #fff;
+    background-color: #39f;
+    cursor: pointer;
+    outline: none;
   }
 </style>
